06739459609f18358f0a091205a51a71f9ca7474
Artem Tarasov <70967111+VaeSemper@users.noreply.github.com>
2022-07-11 15:42:35 +0300

    preparing postgresql

--- backend/foodgram_project/settings.py
+++ backend/foodgram_project/settings.py
@@ -59,6 +59,12 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
+        # 'ENGINE': os.getenv('DB_ENGINE', default='django.db.backends.postgresql'),
+        # 'NAME': os.getenv('DB_NAME', default='mydatabase'),
+        # 'USER': os.getenv('DB_USER', default='mydatabaseuser'),
+        # 'PASSWORD': os.getenv('DB_PASSWORD', default='mypassword'),
+        # 'HOST': os.getenv('DB_HOST', default='127.0.0.1'),
+        # 'PORT': os.getenv('DB_PORT', default='5432'),
     }
 }
 
