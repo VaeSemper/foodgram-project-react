d4444992c7bd767dc02edd18d9ebea41029f42da
Artem Tarasov <70967111+VaeSemper@users.noreply.github.com>
2022-07-18 13:57:26 +0300

    edited admin list filter for recipes, ingredients

--- backend/recipes/admin.py
+++ backend/recipes/admin.py
@@ -21,7 +21,7 @@ class TagsAdmin(admin.ModelAdmin):
 @admin.register(Ingredients)
 class IngredientsAdmin(admin.ModelAdmin):
     list_display = ('name', 'measurement_unit')
-    list_filter = ('measurement_unit',)
+    list_filter = ('name', 'measurement_unit',)
     list_per_page = LINES_PER_PAGE
     search_fields = ('name',)
 
@@ -40,7 +40,7 @@ class IngredientInRecipeAdmin(admin.ModelAdmin):
 class RecipesAdmin(admin.ModelAdmin):
     list_display = ('name', 'author', 'recipe_description', 'cooking_time',
                     'pub_date',)
-    list_filter = ('author',)
+    list_filter = ('name', 'author', 'tags',)
     list_per_page = LINES_PER_PAGE
     search_fields = ('name',)
 
