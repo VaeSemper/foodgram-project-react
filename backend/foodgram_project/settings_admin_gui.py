f47a9f8863fa9547b3a9ac441c18566745ba65dc
Artem Tarasov <70967111+VaeSemper@users.noreply.github.com>
2022-07-21 19:35:59 +0300

    refactored jazzmin settings

---
+++ backend/foodgram_project/settings_admin_gui.py
@@ -0,0 +1,37 @@
+JAZZMIN_SETTINGS = {
+    'welcome_sign': 'Welcome Home, ashen one.',
+    'copyright': 'VaeSemper',
+}
+
+JAZZMIN_UI_TWEAKS = {
+    'navbar_small_text': False,
+    'footer_small_text': True,
+    'body_small_text': False,
+    'brand_small_text': True,
+    'brand_colour': 'navbar-secondary',
+    'accent': 'accent-primary',
+    'navbar': 'navbar-dark',
+    'no_navbar_border': False,
+    'navbar_fixed': True,
+    'layout_boxed': False,
+    'footer_fixed': False,
+    'sidebar_fixed': True,
+    'sidebar': 'sidebar-dark-primary',
+    'sidebar_nav_small_text': True,
+    'sidebar_disable_expand': False,
+    'sidebar_nav_child_indent': False,
+    'sidebar_nav_compact_style': False,
+    'sidebar_nav_legacy_style': False,
+    'sidebar_nav_flat_style': True,
+    'theme': 'darkly',
+    'dark_mode_theme': None,
+    'button_classes': {
+        'primary': 'btn-primary',
+        'secondary': 'btn-secondary',
+        'info': 'btn-outline-info',
+        'warning': 'btn-outline-warning',
+        'danger': 'btn-outline-danger',
+        'success': 'btn-outline-success'
+    },
+    'actions_sticky_top': False
+}
